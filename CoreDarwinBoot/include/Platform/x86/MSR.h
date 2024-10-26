0555b582d20ad806f17ca62e9393047ac0a92fa2
Zormeister <57213163+Zormeister@users.noreply.github.com>
2024-10-27 10:19:52 +1100

    define some HW MSRs

--- CoreDarwinBoot/include/Platform/x86/MSR.h
+++ CoreDarwinBoot/include/Platform/x86/MSR.h
@@ -14,3 +14,16 @@
  */
 
 #pragma once
+
+/* 64-bit */
+#define MSR_P5_TSC 0x00000010
+
+#define MSR_MPERF 0x000000E7
+#define MSR_APERF 0x000000E8
+
+#define MSR_TSC_RATIO 0xC0000104
+#define TSC_RATE_INTEGER_SHIFT 32
+#define TSC_RATE_INTEGER_MASK (1 << 32) | (1 << 33) | (1 << 33) | (1 << 34) | (1 << 35) | (1 << 36) | (1 << 37) | (1 << 38) | (1 << 38) | (1 << 39)
+
+#define MSR_AMD_HW_CONFIG 0xC0010015
+#define AMD_HW_CONFIG_TSC_LOCK (1 << 21)
